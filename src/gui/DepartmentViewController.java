f94fbceb86ff6dc52cc89680bbca8c0fe55ebc57
FabricioLima-C <fabriciocabral07@gmail.com>
2020-03-20 13:28:40 -0300

    DepartmentList Controller

---
+++ src/gui/DepartmentViewController.java
@@ -0,0 +1,46 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+
+public class DepartmentViewController implements Initializable {
+	
+	@FXML
+	private Button btNew;
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		//padrão do javafx para inicializar as colunas
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		// getWindow pega a referência da janela, ele é uma superClasse do Stage
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+
+}
