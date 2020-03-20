00e4301406d9e9625e2e08991570501a3a18383e
FabricioLima-C <fabriciocabral07@gmail.com>
2020-03-20 12:02:20 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 	
@@ -28,12 +37,35 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/about.fxml");
 	}
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 	}
-
+	
+	// Multi thread
+	private void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVbox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			//O método .getRoot() pega o primeiro elemento da View
+			VBox mainVBox = ((VBox) ((ScrollPane) mainScene.getRoot()).getContent());
+			//Crio um nó, e guardo o primeiro elemento da VBox (MenuBar)
+			Node mainMenu = mainVBox.getChildren().get(0);
+			//limpo todos os filhos da tela anterior
+			mainVBox.getChildren().clear();
+			//adiciono o MenuBar
+			mainVBox.getChildren().add(mainMenu);
+			//adiciono todos os filhos da nova view
+			mainVBox.getChildren().addAll(newVbox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlerts("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
 }
