efc321ec596d8c01d92b2979b76525e07400c244
FabricioLima-C <fabriciocabral07@gmail.com>
2020-03-31 21:42:56 -0300

    SellerList

---
+++ src/gui/SellerListController.java
@@ -0,0 +1,183 @@
+package gui;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import application.Main;
+import db.DbIntegrityException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Seller;
+import model.services.SellerService;
+
+public class SellerListController implements Initializable, DataChangeListener {
+	
+	private SellerService service;
+	
+	@FXML
+	private Button btNew;
+	@FXML
+	private TableView<Seller> tableViewSeller;
+	@FXML
+	private TableColumn<Seller, Integer> tableColumnId;
+	@FXML
+	private TableColumn<Seller, String> tableColumnName;
+	@FXML
+	private TableColumn<Seller, Seller> tableColumnEdit;
+	@FXML
+	private TableColumn<Seller, Seller> tableColumnRemove;
+	
+	private ObservableList<Seller> obsList;
+	
+	//Um argumento é referenciado, para obter a referencia do controle que recebe o evento
+	@FXML
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		Seller obj = new Seller();
+		createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		//padrão do javafx para inicializar as colunas
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		// getWindow pega a referência da janela, ele é uma superClasse do Stage
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		//macete para fazer a tableview acompanhar o tamanho da tela
+		tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
+	// Inversão de controle, injetando a dependência
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Seller> list = service.findAll();
+		// Instância a obslist e carrega os dados nela
+		obsList = FXCollections.observableArrayList(list);
+		tableViewSeller.setItems(obsList);
+		initEditButtons();
+		initRemoveButtons();
+	}
+	
+	private void createDialogForm(Seller obj, String absoluteName,Stage parentStage) {
+//		try {
+//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+//			Pane pane = loader.load();
+//			
+//			SellerFormController controller = loader.getController();
+//			controller.setSeller(obj);
+//			controller.setDeparmentService(new SellerService());
+//			controller.subscribeDataChangeListener(this);
+//			controller.updateFormData();
+//			
+//			// Quando carregar uma janela de dialogo modal, deve-se instanciar um novo Stage(palco)
+//			Stage dialogStage = new Stage();
+//			dialogStage.setTitle("Enter Seller data");
+//			// criar uma nova cena, com elemento raiz usando o Pane
+//			dialogStage.setScene(new Scene(pane));
+//			//A janela pode ou não ser redimencionada
+//			dialogStage.setResizable(false);
+//			//Diz quem é o pai dessa janela
+//			dialogStage.initOwner(parentStage);
+//			//Método que diz se a janela é modal ou se tem outro comportamento, 
+//			//enquanto não fechar ela, não podera acessar outra janela
+//			dialogStage.initModality(Modality.WINDOW_MODAL);
+//			dialogStage.showAndWait();
+//		}
+//		catch(IOException e) {
+//			Alerts.showAlerts("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+//		}
+	}
+	
+	private void initEditButtons() {
+		tableColumnEdit.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEdit.setCellFactory(param -> new TableCell<Seller, Seller>(){
+			private final Button button = new Button("edit");
+			
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if(obj == null) {
+					setGraphic(null);
+					return;
+				}
+				
+				setGraphic(button);
+				button.setOnAction(event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+			}
+		});
+	}
+	
+	private void initRemoveButtons() {
+		tableColumnRemove.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnRemove.setCellFactory(param -> new TableCell<Seller, Seller>(){
+			private final Button button = new Button("remove");
+			
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if(obj == null) {
+					setGraphic(null);
+					return;
+				}
+				
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+		});
+	}
+	
+	private void removeEntity(Seller obj) {
+		//A partir do objeto Optional, ele cria uma caixa de dialo perguntando se você confirma ou não alguma operação
+		Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete");
+		
+		//O resultando sendo OK
+		if(result.get() == ButtonType.OK) {
+			if(service == null) {
+				throw new IllegalStateException("Service was null");
+			}
+			try {
+				service.remove(obj);
+				updateTableView();
+			}
+			//Capturar uma possível exceção relacionada a deleção do objeto
+			catch(DbIntegrityException e) {
+				Alerts.showAlerts("Error removing object", null, e.getMessage(), AlertType.ERROR);
+			}
+		}
+		
+	}
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+	}
+	
+}
